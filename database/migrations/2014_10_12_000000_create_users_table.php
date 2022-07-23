f251ef40fad61b1ad9c817b180ab0156ff806034
Natalia <nati8906@gmail.com>
2022-07-23 12:56:18 -0400

    add migrations columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->unsignedInteger('persona_id')->unique();
+            $table->foreign('persona_id')->references('id')->on('personas');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
         });
     }
 
